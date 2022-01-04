35a04aaa99552ea1bc6a133a0aa184d233888a09
SilhouettesForYou <mgze.wang@gmail.com>
2022-01-04 17:02:58 +0800

    feat✨:  服务器导表增加表头枚举映射

--- csv_to_lua.py
+++ csv_to_lua.py
@@ -20,50 +20,50 @@ class CSVToLua:
         RESET = '\033[0m' #RESET COLOR
 
 
-    # class TableTypeEnum:
-    #     TYPES = {
-    #         'NONE' : -1,
-    #         'CHAR' : 0,
-    #         'VECTOR_CHAR' : 100,
-    #         'SEQUENCE_CHAR' : 200,
-    #         'VECTOR_SEQUENCE_CHAR' : 300,
-    #         'VECTOR_VECTOR_CHAR' : 400,
-    #         'BOOL' : 1,
-    #         'VECTOR_BOOL' : 101,
-    #         'SEQUENCE_BOOL' : 201,
-    #         'VECTOR_SEQUENCE_BOOL' : 301,
-    #         'VECTOR_VECTOR_BOOL' : 401,
-    #         'INT' : 2,
-    #         'VECTOR_INT' : 102,
-    #         'SEQUENCE_INT' : 202,
-    #         'VECTOR_SEQUENCE_INT' : 302,
-    #         'VECTOR_VECTOR_INT' : 402,
-    #         'UINT' : 3,
-    #         'VECTOR_UINT' : 103,
-    #         'SEQUENCE_UINT' : 203,
-    #         'VECTOR_SEQUENCE_UINT' : 303,
-    #         'VECTOR_VECTOR_UINT' : 403,
-    #         'FLOAT' : 4,
-    #         'VECTOR_FLOAT' : 104,
-    #         'SEQUENCE_FLOAT' : 204,
-    #         'VECTOR_SEQUENCE_FLOAT' : 304,
-    #         'VECTOR_VECTOR_FLOAT' : 404,
-    #         'DOUBLE' : 5,
-    #         'VECTOR_DOUBLE' : 105,
-    #         'SEQUENCE_DOUBLE' : 205,
-    #         'VECTOR_SEQUENCE_DOUBLE' : 305,
-    #         'VECTOR_VECTOR_DOUBLE' : 405,
-    #         'LONGLONG' : 6,
-    #         'VECTOR_LONGLONG' : 106,
-    #         'SEQUENCE_LONGLONG' : 206,
-    #         'VECTOR_SEQUENCE_LONGLONG' : 306,
-    #         'VECTOR_VECTOR_LONGLONG' : 406,
-    #         'STRING' : 7,
-    #         'VECTOR_STRING' : 107,
-    #         'SEQUENCE_STRING' : 207,
-    #         'VECTOR_SEQUENCE_STRING' : 307,
-    #         'VECTOR_VECTOR_STRING' : 407,
-    #     }
+    class TableTypeEnum:
+        TYPES = {
+            'NONE' : -1,
+            'CHAR' : 0,
+            'VECTOR_CHAR' : 100,
+            'SEQUENCE_CHAR' : 200,
+            'VECTOR_SEQUENCE_CHAR' : 300,
+            'VECTOR_VECTOR_CHAR' : 400,
+            'BOOL' : 1,
+            'VECTOR_BOOL' : 101,
+            'SEQUENCE_BOOL' : 201,
+            'VECTOR_SEQUENCE_BOOL' : 301,
+            'VECTOR_VECTOR_BOOL' : 401,
+            'INT' : 2,
+            'VECTOR_INT' : 102,
+            'SEQUENCE_INT' : 202,
+            'VECTOR_SEQUENCE_INT' : 302,
+            'VECTOR_VECTOR_INT' : 402,
+            'UINT' : 3,
+            'VECTOR_UINT' : 103,
+            'SEQUENCE_UINT' : 203,
+            'VECTOR_SEQUENCE_UINT' : 303,
+            'VECTOR_VECTOR_UINT' : 403,
+            'FLOAT' : 4,
+            'VECTOR_FLOAT' : 104,
+            'SEQUENCE_FLOAT' : 204,
+            'VECTOR_SEQUENCE_FLOAT' : 304,
+            'VECTOR_VECTOR_FLOAT' : 404,
+            'DOUBLE' : 5,
+            'VECTOR_DOUBLE' : 105,
+            'SEQUENCE_DOUBLE' : 205,
+            'VECTOR_SEQUENCE_DOUBLE' : 305,
+            'VECTOR_VECTOR_DOUBLE' : 405,
+            'LONGLONG' : 6,
+            'VECTOR_LONGLONG' : 106,
+            'SEQUENCE_LONGLONG' : 206,
+            'VECTOR_SEQUENCE_LONGLONG' : 306,
+            'VECTOR_VECTOR_LONGLONG' : 406,
+            'STRING' : 7,
+            'VECTOR_STRING' : 107,
+            'SEQUENCE_STRING' : 207,
+            'VECTOR_SEQUENCE_STRING' : 307,
+            'VECTOR_VECTOR_STRING' : 407,
+        }
     
 
     def __init__(self):
@@ -274,6 +274,15 @@ class CSVToLua:
         return '{}'
 
 
+    def type_map_compile(self, _type):
+        patterns = re.compile('[a-zA-Z]+').findall(_type.lower())
+        for k, v in self.TableTypeEnum.TYPES.items():
+            split = k.split('_')
+            if len(patterns) == len(split) and len(re.compile('|'.join(patterns)).findall(k.lower())) == len(split):
+                return v
+        return -1
+
+
     def compress_lua(self, obj, name):
         s = 'local t = {}\n'
 
@@ -317,6 +326,11 @@ class CSVToLua:
 
         # define table type enum for server
         if self.pos == 'server':
+            s += '\n\nlocal __table_type_enum = {'
+            for key in sorted(self.heads.items(), key = lambda item : item[0]):
+                enum = self.type_map_compile(self.types[key[1]]['FieldTypeName'])
+                s += '{}={},'.format(key[1], enum)
+            s = s[:-1] + '}\n'
 
 
         # add postfix
@@ -327,7 +341,10 @@ class CSVToLua:
         s += '\tend\n'
         s += '\tbase.__metatable = false\n'
         s += 'end\n'
-        s += 'local {} = t'.format(name)
+        if self.pos == 'server':
+            s += 'local {} = {{header=__table_type_enum, data=t}}'.format(name)
+        else:
+            s += 'local {} = t'.format(name)
         s += '\nreturn {}\n'.format(name)
 
         return s
